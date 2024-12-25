632870fea2f3417fa3b45fd0816d52dcab2f3b19
Artem Khvostyk <artem.khvostyk.email@gmail.com>
2024-12-25 18:04:16 +0100

    feat: add tiptap extensions

--- src/app/documents/[id]/components/editor.tsx
+++ src/app/documents/[id]/components/editor.tsx
@@ -2,23 +2,44 @@
 
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
+import Table from '@tiptap/extension-table';
+import TableCell from '@tiptap/extension-table-cell';
+import TableHeader from '@tiptap/extension-table-header';
+import TableRow from '@tiptap/extension-table-row';
+import ImageResize from 'tiptap-extension-resize-image';
 
 const Editor = () => {
   const editor = useEditor({
+    immediatelyRender: false,
     editorProps: {
       attributes: {
         style: 'padding-inline: 56px',
-        class:
-          'focus:outline-none print:border-0 bg-white border border-[#c7c7c7] flex' +
-          'flex-column min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
+        class: `focus:outline-none print:border-0 bg-white border\
+          border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px]\
+          pt-10 pr-14 pb-10 cursor-text`,
       },
     },
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      ImageResize,
+    ],
     content: '<p>Hello, World! 🌍</p>',
   });
 
   return (
-    <div className='size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:overflow-visible'>
+    <div className='size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:overflow-visible flex-col'>
       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent editor={editor} />
       </div>
