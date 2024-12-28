9089f9a9ea4aa63af23b00c79e39d6816bb71be7
Artem Khvostyk <artem.khvostyk.email@gmail.com>
2024-12-28 21:53:03 +0100

    chore: move tiptap extensions to separate file

--- src/app/documents/[id]/components/editor.tsx
+++ src/app/documents/[id]/components/editor.tsx
@@ -1,60 +1,10 @@
 'use client';
 
 import { useEditor, EditorContent } from '@tiptap/react';
-import { StarterKit } from '@tiptap/starter-kit';
-import { Link } from '@tiptap/extension-link';
-import { TextAlign } from '@tiptap/extension-text-align';
-import { Underline } from '@tiptap/extension-underline';
-import { FontFamily } from '@tiptap/extension-font-family';
-import { TextStyle } from '@tiptap/extension-text-style';
-import { TaskItem } from '@tiptap/extension-task-item';
-import { TaskList } from '@tiptap/extension-task-list';
-import { Table } from '@tiptap/extension-table';
-import { TableCell } from '@tiptap/extension-table-cell';
-import { TableHeader } from '@tiptap/extension-table-header';
-import { TableRow } from '@tiptap/extension-table-row';
-import { ImageResize } from 'tiptap-extension-resize-image';
-import { Color } from '@tiptap/extension-color';
-import { Highlight } from '@tiptap/extension-highlight';
 
-import { FontSize } from '@/lib/tiptap/font-size.extension';
-import { LineHeight } from '@/lib/tiptap/line-height.extension';
 import { useEditorStore } from '@/store/use-editor-store';
-
-const extensions = [
-  StarterKit,
-  Link.configure({
-    openOnClick: false,
-    autolink: true,
-    defaultProtocol: 'https',
-  }),
-  TextAlign.configure({
-    types: ['heading', 'paragraph'],
-  }),
-  Underline,
-  FontFamily,
-  TextStyle,
-  TaskList,
-  TaskItem.configure({
-    nested: true,
-  }),
-  Table.configure({
-    resizable: true,
-  }),
-  TableRow,
-  TableHeader,
-  TableCell,
-  ImageResize,
-  Color,
-  Highlight.configure({
-    multicolor: true,
-  }),
-  FontSize,
-  LineHeight.configure({
-    types: ['heading', 'paragraph'],
-    defaultLineHeight: 'normal',
-  }),
-];
+import { editorExtensions } from '../lib/tiptap';
+import { Ruller } from './ruller';
 
 const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -68,7 +18,7 @@ const Editor = () => {
           pt-10 pr-14 pb-10 cursor-text`,
       },
     },
-    extensions,
+    extensions: editorExtensions,
     content: '<p>Hello, World! 🌍</p>',
     onCreate({ editor }) {
       setEditor(editor);
@@ -80,6 +30,7 @@ const Editor = () => {
 
   return (
     <div className='size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:overflow-visible flex-col'>
+      <Ruller />
       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent editor={editor} />
       </div>
---
+++ src/app/documents/[id]/lib/tiptap.ts
@@ -0,0 +1,55 @@
+import { StarterKit } from '@tiptap/starter-kit';
+import { Link } from '@tiptap/extension-link';
+import { TextAlign } from '@tiptap/extension-text-align';
+import { Underline } from '@tiptap/extension-underline';
+import { FontFamily } from '@tiptap/extension-font-family';
+import { TextStyle } from '@tiptap/extension-text-style';
+import { TaskItem } from '@tiptap/extension-task-item';
+import { TaskList } from '@tiptap/extension-task-list';
+import { Table } from '@tiptap/extension-table';
+import { TableCell } from '@tiptap/extension-table-cell';
+import { TableHeader } from '@tiptap/extension-table-header';
+import { TableRow } from '@tiptap/extension-table-row';
+import { ImageResize } from 'tiptap-extension-resize-image';
+import { Color } from '@tiptap/extension-color';
+import { Highlight } from '@tiptap/extension-highlight';
+
+import { FontSize } from '@/lib/tiptap/font-size.extension';
+import { LineHeight } from '@/lib/tiptap/line-height.extension';
+
+const editorExtensions = [
+  StarterKit,
+  Link.configure({
+    openOnClick: false,
+    autolink: true,
+    defaultProtocol: 'https',
+  }),
+  TextAlign.configure({
+    types: ['heading', 'paragraph'],
+  }),
+  Underline,
+  FontFamily,
+  TextStyle,
+  TaskList,
+  TaskItem.configure({
+    nested: true,
+  }),
+  Table.configure({
+    resizable: true,
+  }),
+  TableRow,
+  TableHeader,
+  TableCell,
+  ImageResize,
+  Color,
+  Highlight.configure({
+    multicolor: true,
+  }),
+  FontSize,
+  LineHeight.configure({
+    types: ['heading', 'paragraph'],
+    defaultLineHeight: 'normal',
+  }),
+];
+
+export { editorExtensions };
