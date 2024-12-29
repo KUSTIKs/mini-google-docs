077ff914d61d388826020ffe2179ec9d21d15d2c
Artem Khvostyk <artem.khvostyk.email@gmail.com>
2024-12-29 20:06:30 +0100

    feat: add tools to navbar

--- src/app/documents/[id]/components/navbar.tsx
+++ src/app/documents/[id]/components/navbar.tsx
@@ -35,10 +35,85 @@ import {
   MenubarSubTrigger,
   MenubarTrigger,
 } from '@/components/ui/menubar';
+import { useEditorStore } from '@/store/use-editor-store';
 
 const Navbar = () => {
+  const { editor } = useEditorStore();
+
   const printPage = () => {
     window.print();
+  };
+
+  const undo = () => {
+    editor?.chain().focus().undo().run();
+  };
+  const redo = () => {
+    editor?.chain().focus().redo().run();
+  };
+
+  const insertTable = ({ rows, cols }: { rows: number; cols: number }) => {
+    const options = { rows, cols, withHeaderRow: false };
+    editor?.chain().focus().insertTable(options).run();
+  };
+
+  const toggleBold = () => {
+    editor?.chain().focus().toggleBold().run();
+  };
+  const toggleItalic = () => {
+    editor?.chain().focus().toggleItalic().run();
+  };
+  const toggleUnderline = () => {
+    editor?.chain().focus().toggleUnderline().run();
+  };
+  const toggleStrike = () => {
+    editor?.chain().focus().toggleStrike().run();
+  };
+  const clearFormatting = () => {
+    editor?.chain().focus().unsetAllMarks().run();
+  };
+
+  const downloadFile = (blob: Blob, filename: string) => {
+    const url = URL.createObjectURL(blob);
+
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = filename;
+
+    anchor.click();
+  };
+
+  const downloadJson = () => {
+    if (!editor) return;
+
+    const content = editor.getJSON();
+    const blob = new Blob([JSON.stringify(content)], {
+      type: 'application/json',
+    });
+
+    downloadFile(blob, 'document.json');
+  };
+  const downloadHtml = () => {
+    if (!editor) return;
+
+    const content = editor.getHTML();
+    const blob = new Blob([content], {
+      type: 'text/html',
+    });
+
+    downloadFile(blob, 'document.html');
+  };
+  const downloadText = () => {
+    if (!editor) return;
+
+    const content = editor.getText();
+    const blob = new Blob([content], {
+      type: 'text/plain',
+    });
+
+    downloadFile(blob, 'document.txt');
+  };
+  const downloadPdf = () => {
+    printPage();
   };
 
   return (
@@ -62,19 +137,19 @@ const Navbar = () => {
                       Save
                     </MenubarSubTrigger>
                     <MenubarSubContent>
-                      <MenubarItem>
+                      <MenubarItem onClick={downloadJson}>
                         <FileJsonIcon className='size-4 mr-2' />
                         JSON
                       </MenubarItem>
-                      <MenubarItem>
+                      <MenubarItem onClick={downloadHtml}>
                         <GlobeIcon className='size-4 mr-2' />
                         HTML
                       </MenubarItem>
-                      <MenubarItem>
+                      <MenubarItem onClick={downloadPdf}>
                         <BsFilePdf className='size-4 mr-2' />
                         PDF
                       </MenubarItem>
-                      <MenubarItem>
+                      <MenubarItem onClick={downloadText}>
                         <FileTextIcon className='size-4 mr-2' />
                         Text
                       </MenubarItem>
@@ -111,12 +186,12 @@ const Navbar = () => {
                   Edit
                 </MenubarTrigger>
                 <MenubarContent>
-                  <MenubarItem>
+                  <MenubarItem onClick={undo}>
                     <Undo2Icon className='size-4 mr-2' />
                     Undo
                     <MenubarShortcut>Ctrl+Z</MenubarShortcut>
                   </MenubarItem>
-                  <MenubarItem>
+                  <MenubarItem onClick={redo}>
                     <Redo2Icon className='size-4 mr-2' />
                     Redo
                     <MenubarShortcut>Ctrl+Y</MenubarShortcut>
@@ -131,10 +206,26 @@ const Navbar = () => {
                   <MenubarSub>
                     <MenubarSubTrigger>Table</MenubarSubTrigger>
                     <MenubarSubContent>
-                      <MenubarItem>1 x 1</MenubarItem>
-                      <MenubarItem>2 x 2</MenubarItem>
-                      <MenubarItem>3 x 3</MenubarItem>
-                      <MenubarItem>4 x 4</MenubarItem>
+                      <MenubarItem
+                        onClick={() => insertTable({ rows: 1, cols: 1 })}
+                      >
+                        1 x 1
+                      </MenubarItem>
+                      <MenubarItem
+                        onClick={() => insertTable({ rows: 2, cols: 2 })}
+                      >
+                        2 x 2
+                      </MenubarItem>
+                      <MenubarItem
+                        onClick={() => insertTable({ rows: 3, cols: 3 })}
+                      >
+                        3 x 3
+                      </MenubarItem>
+                      <MenubarItem
+                        onClick={() => insertTable({ rows: 4, cols: 4 })}
+                      >
+                        4 x 4
+                      </MenubarItem>
                     </MenubarSubContent>
                   </MenubarSub>
                 </MenubarContent>
@@ -150,22 +241,22 @@ const Navbar = () => {
                       Text
                     </MenubarSubTrigger>
                     <MenubarSubContent>
-                      <MenubarItem>
+                      <MenubarItem onClick={toggleBold}>
                         <BoldIcon className='size-4 mr-2' />
                         Bold
                         <MenubarShortcut>Ctrl+B</MenubarShortcut>
                       </MenubarItem>
-                      <MenubarItem>
+                      <MenubarItem onClick={toggleItalic}>
                         <ItalicIcon className='size-4 mr-2' />
                         Italic
                         <MenubarShortcut>Ctrl+I</MenubarShortcut>
                       </MenubarItem>
-                      <MenubarItem>
+                      <MenubarItem onClick={toggleUnderline}>
                         <UnderlineIcon className='size-4 mr-2' />
                         <span className='mr-2'>Underline</span>
                         <MenubarShortcut>Ctrl+U</MenubarShortcut>
                       </MenubarItem>
-                      <MenubarItem>
+                      <MenubarItem onClick={toggleStrike}>
                         <StrikethroughIcon className='size-4 mr-2' />
                         <span className='mr-2'>Strikethrough</span>
                         <MenubarShortcut>Ctrl+S</MenubarShortcut>
@@ -173,7 +264,7 @@ const Navbar = () => {
                     </MenubarSubContent>
                   </MenubarSub>
 
-                  <MenubarItem>
+                  <MenubarItem onClick={clearFormatting}>
                     <RemoveFormattingIcon className='size-4 mr-2' />
                     Crear formatting
                   </MenubarItem>
