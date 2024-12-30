380bd8f3c12ce20aeb32f6b0e2a526e26c94e743
Artem Khvostyk <artem.khvostyk.email@gmail.com>
2024-12-30 19:11:10 +0100

    chore: change clerk package to nextjs

--- src/components/provides/convex-client-provider.tsx
+++ src/components/provides/convex-client-provider.tsx
@@ -8,7 +8,7 @@ import {
   Unauthenticated,
 } from 'convex/react';
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
-import { ClerkProvider, SignIn, useAuth } from '@clerk/clerk-react';
+import { ClerkProvider, SignIn, useAuth } from '@clerk/nextjs';
 import { FullscreenLoader } from '../fullscreen-loader';
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
@@ -18,7 +18,7 @@ type Props = {
 };
 
 const ConvexClientProvider = ({ children }: Props) => {
-  const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+  const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
   return (
     <ClerkProvider publishableKey={clerkPublishableKey}>
@@ -26,7 +26,7 @@ const ConvexClientProvider = ({ children }: Props) => {
         <Authenticated>{children}</Authenticated>
         <Unauthenticated>
           <div className='flex flex-col items-center justify-center min-h-screen'>
-            <SignIn />
+            <SignIn routing='hash' />
           </div>
         </Unauthenticated>
         <AuthLoading>
